d13ce21bdcb406257e6da03942d65d1b916e7dfe
Sandeep Kumar <orion10101999@gmail.com>
2024-07-31 11:28:29 +0530

    ok

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import InvoiceForm from '../components/InvoiceForm';
 import InvoiceList from '../components/InvoiceList';
-import Dashboard from '../components/Dashboard';
 import { Link } from 'react-router-dom';
 import Login from './Login';
-import Register from './Register';
 
 const Home = () => {
   const { currentUser } = useSelector((state) => state.user);
@@ -16,12 +14,10 @@ const Home = () => {
         currentUser ? (<>
         <InvoiceForm/>
         <InvoiceList/>
-        <Dashboard/>
         <Link to={'/logout'} className='bg-red-600 py-2 px-4'>LogOut</Link>
         </>):(<>
             <Dashboard/>
             <Login/>
-            <Register/>
         </>)
       }
     </div>
